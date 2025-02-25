15f0fc66e6132558383793069ee07676a1b0b49f
Jose6348 <jfalasco88@gmail.com>
2025-02-25 17:20:22 -0300

    modelagem de dados

--- index.php
+++ index.php
@@ -1,3 +1,8 @@
+<?php
+require 'dados.php';
+?>
+
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -8,9 +13,9 @@
     <script src="https://unpkg.com/@tailwindcss/browser@4"></script>
 </head>
 
-<body class="bg-neutral-800 text-neutral-400">
+<body class="bg-neutral-900 text-neutral-400">
     <header class=" bg-stone-800 ">
-        <nav class="mx-auto max-w-screen-md flex justify-between px-8 py-4">
+        <nav class="mx-auto max-w-screen-lg flex justify-between  py-4">
             <div class="text-bold text-xl tracking-wide">
                 Bibliotech
             </div>
@@ -19,15 +24,53 @@
                 <li><a href="/meus-livros.php " class="hover:underline">Meus Livros</a></li>
             </ul>
             <ul>
-                <li >
+                <li>
                     <a href="/login.php" class="hover:underline">Fazer login </a>
                 </li>
             </ul>
         </nav>
     </header>
 
-    <main>
-        Lista de Livros
+    <main class="mx-auto max-w-screen-lg space-y-10">
+
+        <h1 class="mt-6 font-bold text-lg">Explorar</h1>
+
+        <form action="" class="w-full flex space-x-2 ">
+
+            <input type="text" class="border-stone-800 border-2 rounded-md  bg-stone-900 text-sm focus:outline-none px-2 py-1 w-full"
+                placeholder="Pesquisar..."
+                name="Pesquisar">
+            <button type="submit">🔍</button>
+        </form>
+
+
+        <section class="grid-cols-1 md:grid-cols-2 lg:grid-cols-3 grid gap-4 ">
+            <?php foreach($livros as $livro): ?>
+
+            <div class="  p-2 border-stone-800 border-2 bg-stone-900">
+                <div class=" flex">
+                    <div class="w-1/3">image</div>
+                    <div class="space-y-1">
+                        <a href="/livro.php?id=<?=$livro['id']?>" class="font-semibold hover:underline">
+                        <?=$livro['titulo']?>
+                        </a>
+                        <div class="text-xs italic">
+                        <?=$livro['autor']?>
+                        </div>
+                        <div class="text-xs italic">
+                            ⭐️⭐️⭐️⭐️⭐️ <?=$livro['avaliacoes']?>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="text-sm"><?=$livro['descricao']?></div>
+            </div>
+
+            <?php endforeach; ?>
+
+
+          
+
     </main>
 </body>
 
