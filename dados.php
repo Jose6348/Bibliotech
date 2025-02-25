3a86c209e68982cd893e1a295b8f47b4660c1cc9
Jose6348 <jfalasco88@gmail.com>
2025-02-25 17:20:11 -0300

    modelagem de dados

---
+++ dados.php
@@ -0,0 +1,86 @@
+<?php 
+
+$livros = [
+    [
+        "id" => 1,
+        "titulo" => "O Senhor dos Anéis",
+        "autor" => "J.R.R. Tolkien",
+        "avaliacoes" => 5,
+        "descricao" => "Uma jornada épica pela Terra Média, onde um grupo de heróis deve destruir um anel maligno.",
+        "imagem" => "https://aventurasnahistoria.uol.com.br/media/uploads/o-senhor-dos-aneis-livro.jpg"
+    ],
+    [
+        "id" => 2,
+        "titulo" => "1984",
+        "autor" => "George Orwell",
+        "avaliacoes" => 4.8,
+        "descricao" => "Uma distopia sobre um futuro totalitário onde o Grande Irmão está sempre observando.",
+        "imagem" => "https://produto.mercadolivre.com.br/MLB-1824836782-livro-1984-george-orwell-edico-luxo-capa-dura-_JM"
+    ],
+    [
+        "id" => 3,
+        "titulo" => "Dom Casmurro",
+        "autor" => "Machado de Assis",
+        "avaliacoes" => 4.7,
+        "descricao" => "Um clássico da literatura brasileira, explorando amor, ciúme e memória.",
+        "imagem" => "https://www.educacaoetransformacao.com.br/wp-content/uploads/2018/10/dom-casmurro.jpg"
+    ],
+    [
+        "id" => 4,
+        "titulo" => "O Pequeno Príncipe",
+        "autor" => "Antoine de Saint-Exupéry",
+        "avaliacoes" => 4.9,
+        "descricao" => "Uma história filosófica e encantadora sobre amizade, amor e humanidade.",
+        "imagem" => "https://produto.mercadolivre.com.br/MLB-1866340478-livro-infantis-o-pequeno-principe-edluxo-capa-dura-novo-_JM"
+    ],
+    [
+        "id" => 5,
+        "titulo" => "Harry Potter e a Pedra Filosofal",
+        "autor" => "J.K. Rowling",
+        "avaliacoes" => 5,
+        "descricao" => "O início da saga do jovem bruxo Harry Potter, descobrindo seu verdadeiro destino.",
+        "imagem" => "https://www.wook.pt/livro/harry-potter-e-a-pedra-filosofal-j-k-rowling/24066798"
+    ],
+    [
+        "id" => 6,
+        "titulo" => "A Arte da Guerra",
+        "autor" => "Sun Tzu",
+        "avaliacoes" => 4.6,
+        "descricao" => "Um tratado milenar sobre estratégia e táticas de guerra aplicáveis à vida e negócios.",
+        "imagem" => "https://www.lpm.com.br/site/default.asp?ID=517262&Template=..%2Flivros%2Flayout_produto.asp"
+    ],
+    [
+        "id" => 7,
+        "titulo" => "O Hobbit",
+        "autor" => "J.R.R. Tolkien",
+        "avaliacoes" => 4.8,
+        "descricao" => "A história de Bilbo Bolseiro, um hobbit que se envolve em uma grande aventura.",
+        "imagem" => "https://brunakeyt.github.io/sonho-literario/paginas/hobbit.html"
+    ],
+    [
+        "id" => 8,
+        "titulo" => "Crime e Castigo",
+        "autor" => "Fiódor Dostoiévski",
+        "avaliacoes" => 4.9,
+        "descricao" => "Um jovem estudante comete um crime e enfrenta um dilema moral intenso.",
+        "imagem" => "https://fpaladini.blogspot.com/2014/04/resenha-crime-e-castigo-fiodor-dostoievski.html"
+    ],
+    [
+        "id" => 9,
+        "titulo" => "O Alquimista",
+        "autor" => "Paulo Coelho",
+        "avaliacoes" => 4.7,
+        "descricao" => "A jornada de um pastor em busca de sua lenda pessoal.",
+        "imagem" => "https://hospicionerd.com.br/paulo-coelho-o-alquimista/"
+    ],
+    [
+        "id" => 10,
+        "titulo" => "A Revolução dos Bichos",
+        "autor" => "George Orwell",
+        "avaliacoes" => 4.8,
+        "descricao" => "Uma sátira política sobre poder e corrupção.",
+        "imagem" => "https://loucasporlivros.blogspot.com/2013/04/resenha-revolucao-dos-bichos-george.html"
+    ]
+];
+
+?>
