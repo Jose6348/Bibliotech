59ef3f1fc24af4f60df5bbbd15ea5488565e2525
Jose6348 <jfalasco88@gmail.com>
2025-02-25 15:07:32 -0300

    Navbar

--- index.php
+++ index.php
@@ -1,6 +1,34 @@
-<?php 
+<!DOCTYPE html>
+<html lang="en">
 
-echo "Hello Word"
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Bibliotech</title>
+    <script src="https://unpkg.com/@tailwindcss/browser@4"></script>
+</head>
 
+<body class="bg-neutral-800 text-neutral-400">
+    <header class=" bg-stone-800 ">
+        <nav class="mx-auto max-w-screen-md flex justify-between px-8 py-4">
+            <div class="text-bold text-xl tracking-wide">
+                Bibliotech
+            </div>
+            <ul class="flex space-x-4">
+                <li><a href="/" class="text-emerald-600">Explorar</a></li>
+                <li><a href="/meus-livros.php " class="hover:underline">Meus Livros</a></li>
+            </ul>
+            <ul>
+                <li >
+                    <a href="/login.php" class="hover:underline">Fazer login </a>
+                </li>
+            </ul>
+        </nav>
+    </header>
 
-?>
+    <main>
+        Lista de Livros
+    </main>
+</body>
+
+</html>
