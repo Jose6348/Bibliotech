b2ec480c5d34bc8bd203ae9cfe5943cd0a3f1407
Jose6348 <jfalasco88@gmail.com>
2025-02-26 17:21:55 -0300

    refact index

--- index.php
+++ index.php
@@ -1,14 +1,8 @@
 <?php
 
-$controller = "index";
+require 'functions.php';
 
-if(isset($_SERVER['PATH_INFO'])){
-    $controller = str_replace('/', '', $_SERVER['PATH_INFO']); // Corrigindo erro na chamada de função
-}
-
-// Forma correta de incluir a variável na string:
-require "controllers/" . $controller . ".controller.php";
-
+require 'routes.php';
 ?>
 
 
