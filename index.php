765f44f6eede2926fe8508555b1802b00d33c378
Jose6348 <jfalasco88@gmail.com>
2025-02-26 16:37:39 -0300

    Controllers

--- index.php
+++ index.php
@@ -1,9 +1,14 @@
 <?php
-require 'dados.php';
 
-$view = "index";
+$controller = "index";
 
-require "views/template/app.php";
+if(isset($_SERVER['PATH_INFO'])){
+    $controller = str_replace('/', '', $_SERVER['PATH_INFO']); // Corrigindo erro na chamada de função
+}
+
+// Forma correta de incluir a variável na string:
+require "controllers/" . $controller . ".controller.php";
+
 ?>
 
 
